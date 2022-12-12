90f3dacb0a1cba2e27b60eb294b78452e2d5f890
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-12 10:14:48 +0200

    Created Car service with their unit testing class.

---
+++ simple_car_crud/grails-app/services/simple_car_crud/CarService.groovy
@@ -0,0 +1,18 @@
+package simple_car_crud
+
+import grails.gorm.services.Service
+
+@Service(Car)
+interface CarService {
+
+    Car get(Serializable id)
+
+    List<Car> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Car save(Car car)
+
+}
---
+++ simple_car_crud/src/test/groovy/simple_car_crud/CarSpec.groovy
@@ -0,0 +1,18 @@
+package simple_car_crud
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class CarSpec extends Specification implements DomainUnitTest<Car> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
